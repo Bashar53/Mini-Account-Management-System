b341874e443feb3801e46aa00924635f41d06c1e
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-03 00:25:23 +0600

    AddIdentity Method & configuartion of DB Context updated in program.cs

--- Program.cs
+++ Program.cs
@@ -3,14 +3,19 @@ using Microsoft.EntityFrameworkCore;
 using Mini_Account_Management_System.DbConnection;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                    .AddEntityFrameworkStores<ApplicationDbContext>();
-var ConnectionStrings = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(ConnectionStrings));
 
 var app = builder.Build();
 

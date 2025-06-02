0e5f379a0a2f4b9cc049431b22bbdd4cd9b07734
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-02 22:11:11 +0600

    AddIdentity -- service added in program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNet.Identity.EntityFramework;
+using Mini_Account_Management_System.DbConnection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 

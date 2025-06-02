4b3a6e02a0d93e5f40e5551c1c73b7a1f78b4ba4
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-02 22:07:12 +0600

    ApplicationDbContext class added in DbConnection

---
+++ DbConnection/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Mini_Account_Management_System.DbConnection
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

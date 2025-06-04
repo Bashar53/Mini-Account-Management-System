6abce3a2f75b0e13dc660cf4fbf1e2e8641280e4
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-04 11:47:23 +0600

    Menu Page added

---
+++ Pages/Shared/_Menu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mini_Account_Management_System.Pages.Shared
+{
+    public class _MenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

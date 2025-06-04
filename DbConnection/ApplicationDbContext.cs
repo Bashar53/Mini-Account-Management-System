54525c3f21004a3ebe0fe3c6d52ca5e81d0998c2
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-04 11:40:27 +0600

    AppResources & AppPermissions Table added in context

--- DbConnection/ApplicationDbContext.cs
+++ DbConnection/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Mini_Account_Management_System.Models;
 
 namespace Mini_Account_Management_System.DbConnection
 {
@@ -7,6 +9,9 @@ namespace Mini_Account_Management_System.DbConnection
         public ApplicationDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+        public DbSet<AppResource> AppResources { get; set; }
+        public DbSet<AppPermission> AppPermissions { get; set; }    
     }
 }

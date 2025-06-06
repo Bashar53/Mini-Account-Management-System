edea2ab1bcc76b095fdcea28e6ef8c3e1f89113f
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-06 12:40:22 +0600

    _AccountTree Partial View Added  to see Account Hierarchichal Tree

---
+++ Pages/AccountsChart/_AccountTree.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mini_Account_Management_System.Pages.AccountsChart
+{
+    public class _AccountTreeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

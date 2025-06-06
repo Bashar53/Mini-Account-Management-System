2fbc1df1190999aa345ac4269b494a1bb6df9128
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-06 12:37:18 +0600

    AccountsChart Model Added

---
+++ Models/AccountsChart.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mini_Account_Management_System.Models
+{
+        public class AccountsChart  
+        {
+            [Key]
+            public int AccountId { get; set; }
+            public string AccountName { get; set; } = default!;
+            public int? ParentId { get; set; }
+            public AccountsChart? Parent { get; set; }
+
+            [NotMapped]
+            public List<AccountsChart> Children { get; set; } = new();
+    }
+    
+}

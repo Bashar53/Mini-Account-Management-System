faac0c660e90508d8df708a82fc09353d3551d8c
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-06 12:41:01 +0600

    AccountChartService updated for Create/Update & Delete Operation

---
+++ Service/AccountsChartService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mini_Account_Management_System.DbConnection;
+using Mini_Account_Management_System.Models;
+
+namespace Mini_Account_Management_System.Service
+{
+    public class AccountsChartService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountsChartService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<List<AccountsChart>> GetAccountsChartAsync(
+    string action, int id = 0, string name = null, int? parentId = null)
+        {
+            return await _db.AccountsChart
+                .FromSqlInterpolated($"EXEC dbo.sp_ManageChartOfAccounts {action}, {id}, {name}, {parentId}")
+                .AsNoTracking()
+                .ToListAsync();
+        }
+        public async Task DeleteAccountAsync(int id)
+        {
+            await _db.Database.ExecuteSqlInterpolatedAsync(
+                $"EXEC dbo.sp_ManageChartOfAccounts 'DELETE', {id}, {null}, {null}");
+        }
+    }
+}

d5f888f062709a1dbd932777f15855df41a0cb1f
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-13 00:03:46 +0600

    Voucher Entry Model -- added

---
+++ Models/VoucherEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace Mini_Account_Management_System.Models
+{
+    public class VoucherEntry
+    {
+        public int AccountId { get; set; }
+        public decimal? Debit { get; set; }
+        public decimal? Credit { get; set; }
+    }
+}

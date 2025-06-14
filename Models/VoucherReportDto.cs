8b357e23376fc323fa809eacb52e9eee3c794baa
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-14 14:57:23 +0600

    Voucher Report DTO model added for Report Generate

--- Models/VoucherReportDto.cs
+++ Models/VoucherReportDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Mini_Account_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mini_Account_Management_System.Models
 {
     public class VoucherReportDto
     {
+        [Key]
         public string VoucherType { get; set; } = "";
         public DateTime Date { get; set; }
         public string ReferenceNo { get; set; } = "";
